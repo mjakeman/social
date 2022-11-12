bef5517203106139667c5cd94c869c009b21b531
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-11-12 13:56:25 +1300

    project: Initial Commit

---
+++ fedinaut/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Gtk;
+using Gio;
+
+void OnActivate(Gio.Application app, EventArgs e)
+{
+    Console.WriteLine("Hello!");
+}
+
+var app = Gtk.Application.New("com.mattjakeman.fedinaut", ApplicationFlags.FlagsNone);
+app.OnActivate += OnActivate;
+app.Run();
+
+Console.WriteLine("Hello, World!");
